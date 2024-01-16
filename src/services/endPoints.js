f297b4d4d1fb2dab40698eb146d9e1583fd803bd
Roshni <roshni.dhand@appdeft.biz>
2024-01-16 09:50:51 +0530

    Change end points url

--- src/services/endPoints.js
+++ src/services/endPoints.js
@@ -1,10 +1,10 @@
 const endPoints = {
-    LOGIN: 'api/gym-owner/login',
-    PROFILE: 'api/gym-owner/profile',
-    COMPANY: 'api/gym-owner/company',
-    REASON_CODE: 'api/gym-owner/reason-code/',
+    LOGIN: 'api/user/login',
+    PROFILE: 'api/user/profile',
+    COMPANY: 'api/user/company',
+    REASON_CODE: 'api/user/reason-code/',
     UPLOAD_FILES: 'api/upload',
-    JOB_TITLE: 'api/gym-owner/title/',
-    CLUBS: 'api/gym-owner/clubs/',
+    JOB_TITLE: 'api/user/title/',
+    CLUBS: 'api/user/clubs/',
 };
 export default endPoints;
