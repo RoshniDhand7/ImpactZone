589a327e4b3c8d75cc72a79c59a1b19f6626238a
Manveer <appdeftdev@gmail.com>
2024-09-03 14:58:19 +0530

    old code removed

--- src/components/cards/card.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Card = ({ children }) => {
-  return (
-    <div className=" shadow-2  btn-lightblue border-round  p-2 ">
-      <div className="p-2 ">{children}</div>
-    </div>
-  );
-};
